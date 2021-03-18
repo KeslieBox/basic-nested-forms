4905728e8dfab9671d599fc88e46cab6741a18ff
KeslieBox <kesliebox@gmail.com>
2021-03-18 13:39:40 -0500

    Done.

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,4 +1,8 @@
 class Person < ActiveRecord::Base
   has_many :addresses
+  #this writes a sort of setter method: addresses_attributes= , 
+  #which then when we called new_person.save it created both the 
+  #Person object and the Address objects passed in.
+  accepts_nested_attributes_for :addresses
 
 end
